78143db8fd2ce7d92931be8a769bae361b2c06d9
Dmcdominic <dmcdominic@gmail.com>
2020-11-18 18:28:20 -0500

    Initial Unity project, based on Paolo's distributedAuthorityClient, but excluding all 3D assets - https://github.com/molleindustria/distributedAuthorityClient

---
+++ Assets/Game.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+    
+   
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Oligarchy
+
+A multiplayer game about controlling an authoritarian state with your friends
