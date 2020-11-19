496e7e1c33e42ef66fc6075b688529f8c1a39314
Dmcdominic <dmcdominic@gmail.com>
2020-11-18 22:39:38 -0500

    CardInfo.cs, Card.cs, Card prefab, and some example card scriptable objects

---
+++ Assets/_scripts/Cards/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+	// Fields
+	public CardInfo cardInfo;
+
+
+	// Methods
+	private void Awake() {
+		//Debug.Log(cardInfo.title);
+	}
+}
---
+++ Assets/_scripts/Cards/CardInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// enums
+public enum Quality { LIBERTY, HAPPINESS, MONEY, MILITARY }
+public enum Amount { LOW, MEDIUM, HIGH }
+public enum Locality { LOCAL, ADJACENT, GLOBAL }
+
+
+
+// Info for a specific card
+[CreateAssetMenu(menuName = "custom/CardInfo")]
+public class CardInfo : ScriptableObject {
+	// Fields
+	public string title;
+	public string description;
+
+	public Sprite sprite;
+
+	public List<Impact> impacts;
+}
+
+
+
+// Info for how something will impact a particular quality
+[System.Serializable]
+public struct Impact {
+	public bool decrease;
+	public Amount amount;
+	public Quality quality;
+	public Locality locality;
+}
