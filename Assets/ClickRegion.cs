27489f34b787d47a117f2577b1001ede337c11f5
Streakman <46490210+Streakman@users.noreply.github.com>
2020-11-18 23:54:42 -0500

    added map + card buttons and graph slider

---
+++ Assets/ClickRegion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickRegion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Hey Dom we need to put something here");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
