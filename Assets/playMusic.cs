2f945784efcdabd4cdee7ef2672ce059fb7e2f35
Matthew <matthewlucienkomar@gmail.com>
2020-12-16 12:47:41 -0500

    made credits

---
+++ Assets/playMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class playMusic : MonoBehaviour
+{
+    private NetObject thisNetObj;
+
+    // late init
+    private void Start()
+    {
+        thisNetObj = GetComponentInParent<NetObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thisNetObj.netVariables.qualityStates.states.Min() <= QualityStates.SOFT_MAX * 0.3f)
+        {
+
+        }
+    }
+}
